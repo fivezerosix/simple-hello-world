8392e58dd38ef35d8970bfb5bb2bb39e2ee8ce58
Alejandro Rojas <alejandro.rojas@avantica.net>
2018-10-25 12:31:51 -0600

    App Finished

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-hello-world
 NodeJS backend and React Frontend
+
+Frontend:
+run `npm install -g react-scripts`, then `npm install && npm start`
+
+Backend:
+run `npm install && npm start`
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString(),
+      value: ''
+    };
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => {
+        this.tick();
+        fetch('/hello', {
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        })
+        .then((response) => {
+          return response.json() 
+        })
+        .then((response => {
+          this.setState(response)
+        }));
+      }, 1000
+    );
 
-  state = { value: '',  seconds: 0 };
-
-  tick() {
-    this.setState(prevState => ({
-      seconds: prevState.seconds + 1
-    }));
+   
   }
 
   componentWillUnmount() {
-      clearInterval(this.interval);
+    clearInterval(this.intervalID);
   }
 
-
-  componentDidMount () {
-    fetch('/hello', {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    })
-    .then((response) => {
-      return response.json() 
-    })
-    .then((response => {
-      this.setState(response)
-    }));
-    
-    this.interval = setInterval(this.tick.bind(this),1000);
+  tick() {
+    this.setState({
+      time: new Date().toLocaleString()
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        <p> The Backend Says: </p>
-        <h1> Hello {this.state.value} </h1>
+      <div class='container'> 
+        <div className="App vertical-center">
+          <p> The Backend Says: </p>
+          <h1> Hello  <span class='word'>{this.state.value}</span> </h1>
+        </div>
       </div>
+      
     );
   }
 }
