2cf61731b551e0db8ffaf2de32bb25c215431274
Eric Chaves <erchaves@gmail.com>
2018-10-26 17:13:38 -0600

    switching to razzzle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,66 @@
-import React, { Component } from 'react';
 import './App.css';
 
-
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date().toLocaleString(),
-      value: ''
-    };
-  }
-  componentDidMount() {
-    this.intervalID = setInterval(
-      () => {
-        this.tick();
-        fetch('/hello', {
-          headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-        })
-        .then((response) => {
-          return response.json() 
-        })
-        .then((response => {
-          this.setState(response)
-        }));
-      }, 1000
-    );
-
-   
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  tick() {
-    this.setState({
-      time: new Date().toLocaleString()
-    });
-  }
-
-  render() {
-    return (
-      <div class='container'> 
-        <div className="App vertical-center">
-          <p> The Backend Says: </p>
-          <h1> Hello  <span class='word'>{this.state.value}</span> </h1>
-        </div>
-      </div>
-      
-    );
-  }
-}
+import React from 'react';
+const App = () => <div>Welcome to Razzle.</div>;
 
 export default App;
+
+// import React, { Component } from 'react';
+// import './App.css';
+
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       time: new Date().toLocaleString(),
+//       value: ''
+//     };
+//   }
+//   componentDidMount() {
+//     this.intervalID = setInterval(
+//       () => {
+//         this.tick();
+//         fetch('/hello', {
+//           headers : {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json'
+//            }
+//         })
+//         .then((response) => {
+//           return response.json()
+//         })
+//         .then((response => {
+//           this.setState(response)
+//         }));
+//       }, 1000
+//     );
+
+
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.intervalID);
+//   }
+
+//   tick() {
+//     this.setState({
+//       time: new Date().toLocaleString()
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div class='container'>
+//         <div className="App vertical-center">
+//           <p> The Backend Says: </p>
+//           <h1> Hello  <span class='word'>{this.state.value}</span> </h1>
+//         </div>
+//       </div>
+
+//     );
+//   }
+// }
+
+// export default App;
